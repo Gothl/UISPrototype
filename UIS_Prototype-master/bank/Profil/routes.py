4a6f3552ddac77f47a93459c7d695670aff4809b
Gothl <gothlip@hotmail.com>
2020-06-04 13:00:05 +0200

    Arbejder på indsigelse

--- UIS_Prototype-master/bank/Profil/routes.py
+++ UIS_Prototype-master/bank/Profil/routes.py
@@ -1,11 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from bank import app, conn, bcrypt
-from bank.forms import DepositForm, InvestForm
+from bank.forms import DepositForm, InvestForm, IndsigelsesForm
 from flask_login import current_user
 from bank.models import CheckingAccount, InvestmentAccount, update_CheckingAccount
 from bank.models import select_investments_with_certificates, select_investments, select_investments_certificates_sum, select_diagnoser, select_indsigelser
 import sys, datetime
-from bank.models import Indsigelser, update_indsigelse
+from bank.models import Indsigelser, update_indsigelser
 
 Profil = Blueprint('Profil', __name__)
 
@@ -16,13 +16,16 @@ def diagnoser():
         return redirect(url_for('Login.login'))
     #form = InvestForm()
     #investments = select_investments(current_user.CPR_number)
-    investment_certificates = select_investments_with_certificates(current_user.get_id())
-    investment_sums = select_investments_certificates_sum(current_user.get_id())
+    #investment_certificates = select_investments_with_certificates(current_user.get_id())
+    #investment_sums = select_investments_certificates_sum(current_user.get_id())
     diagnoser_allergier = select_diagnoser(current_user.get_id())
     indsigelser = select_indsigelser(current_user.get_id())
-    #indsigelser = opret_indsigelse(current_user.get_id())
-    return render_template('diagnoser.html', title='Diagnoser', inv_cd_list=investment_certificates
-    , inv_sums=investment_sums, diagnoser=diagnoser_allergier, inds=indsigelser)
+    indsigelsesform = IndsigelsesForm()
+    if indsigelsesform.validate_on_submit():
+        indsigelsestekst = indsigelsesform.indsigelsestekst.data
+        ny_indsigelse = update_indsigelser(diagnoseid, now(), indsigelsestekst)
+
+    return render_template('diagnoser.html', title='Diagnoser', diagnoser=diagnoser_allergier, inds=indsigelser, nyins=ny_indsigelse) #inv_cd_list=investment_certificates  , inv_sums=investment_sums,
 
 
 # @Profil.route("/deposit", methods=['GET', 'POST'])
@@ -50,23 +53,25 @@ def summary():
         return redirect(url_for('Login.home'))
     return render_template('deposit.html', title='Deposit', form=form)
 
-@Profil.route("/Håndtér indsigelse", methods=['GET', 'POST'])
+@Profil.route("/indsigelse", methods=['GET', 'POST'])
 def handterIndsigelse():
     if not current_user.is_authenticated:
-        flash('Please Login.','danger')
+        flash('Log venligst ind.','danger')
         return redirect(url_for('Login.login'))
-    form = TransferForm()
-    if form.validate_on_submit():
-        indsigelse=form.indsigelse.data
-        cur = conn.cursor()
-        sql = """
-        UPDATE Indsigelse
-        SET Indsigelse = %s
-        WHERE CPR_number = %s
-        """ 
-        cur.execute(sql, (diagnose_id, CPR_number))
-        conn.commit()
-        cur.close()
-        flash('Transfer succeed!', 'success')
+    indsigelsesform = IndsigelsesForm()
+    if indsigelsesform.validate_on_submit():
+        indsigelse=indsigelsesform.indsigelsestekst.data
+        ny_indsigelse=update_indsigelser(diagnoseid, now(), indsigelse)
+        #indsigelses_id =
+        #cur = conn.cursor()
+        #sql = """
+        #UPDATE Indsigelse
+        #SET Indsigelse = %s
+        #WHERE CPR_number = %s
+        #"""
+        #ur.execute(sql, (diagnose_id, CPR_number))
+        #conn.commit()
+        #cur.close()
+        #flash('Transfer succeed!', 'success')
         return redirect(url_for('Login.home'))
-    return render_template('transfer.html', title='Transfer', form=form)
+    return render_template('indsigelse.html', title='Indsigelse', form=indsigelsesform, )
