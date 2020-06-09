c893b0dc114dc60cb26a7c62d2ce62d6930ad74d
Gothl <gothlip@hotmail.com>
2020-06-09 06:05:13 +0200

    removed our predefined diagnose_ids + adding some extra files (apparently)

--- UIS_Prototype-master/MinSP/schema_ins.sql
+++ UIS_Prototype-master/MinSP/schema_ins.sql
@@ -10,24 +10,23 @@ INSERT INTO public.profiler(cpr_nr, password, fornavn, efternavn, e_mail) VALUES
 INSERT INTO public.profiler(cpr_nr, password, fornavn, efternavn, e_mail) VALUES (1234567894, '$2b$12$KFkp1IEMGT4QrWwjPGhE3ejOv6Z3pYhx/S4qOoFbanR2sMiZqgeJO', 'Barbie', 'Plastik', 'BarbieQueeeen@gmail.com');
 
 --Diagnoser
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78903, 1234567890, '2020-05-15', FALSE, 'COVID-19'); --Diagnose_id + cpr =primary key
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78901, 1234567890, '2020-04-01', TRUE, 'Forhøjet blodtryk');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78902, 1234567890, '2020-05-01', FALSE, 'Slidgigt');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567890, '2020-05-15', FALSE, 'COVID-19');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567890, '2020-04-01', TRUE, 'Forhøjet blodtryk');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567890, '2020-05-01', FALSE, 'Slidgigt');
 
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78911, 1234567891, '2019-03-01', FALSE, 'Grå Stær');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78912, 1234567891, '2020-04-01', TRUE, 'Alkoholisme');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567891, '2019-03-01', FALSE, 'Grå Stær');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567891, '2020-04-01', TRUE, 'Alkoholisme');
 
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78923, 1234567892, '2020-05-02', FALSE, 'Diskusprolaps');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78921, 1234567892, '2018-04-02', FALSE, 'Ledgigt');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78922, 1234567892, '2019-03-01', FALSE, 'Grå stær');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567892, '2020-05-02', FALSE, 'Diskusprolaps');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567892, '2018-04-02', FALSE, 'Ledgigt');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567892, '2019-03-01', FALSE, 'Grå stær');
 
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78932, 1234567893, '2020-04-01', FALSE, 'Diabetes 2');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78931, 1234567893, '2020-03-01', FALSE, 'Slidgigt');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567893, '2020-04-01', FALSE, 'Diabetes 2');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567893, '2020-03-01', FALSE, 'Slidgigt');
 
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78942, 1234567894, '2020-04-15', FALSE, 'Slidgigt');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78943, 1234567894, '2020-05-15', FALSE, 'COVID-19');
-INSERT INTO public.diagnoser_allergier(diagnose_id, cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (78941, 1234567894, '2020-03-15', FALSE, 'Ledgigt');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567894, '2020-04-15', FALSE, 'Slidgigt');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567894, '2020-05-15', FALSE, 'COVID-19');
+INSERT INTO public.diagnoser_allergier(cpr_nr, dato, indsigelse, diagnose_allergi_navn) VALUES (1234567894, '2020-03-15', FALSE, 'Ledgigt');
 
 --Indsigelser
-INSERT INTO public.indsigelser(diagnose_id, dato, indsigelses_tekst) VALUES (78901, now(), 'Jeg er uforstående overfor min diagnose - ret venligst dette.');
-INSERT INTO public.indsigelser(diagnose_id, dato, indsigelses_tekst) VALUES (78912, now(), 'Jeg er uforstående overfor min diagnose - ret venligst dette.');
+INSERT INTO public.indsigelser(diagnose_id, dato, indsigelses_tekst) VALUES (2, now(), 'Jeg er uforstående overfor min diagnose - ret venligst dette.');
